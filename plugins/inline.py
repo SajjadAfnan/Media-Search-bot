3342327b541fe978e6b712392a146cc88b7f28d2
sajjadafnan <71611618+sajjadafnan@users.noreply.github.com>
2021-09-13 19:37:01 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -1,9 +1,12 @@
 import logging
+from urllib.parse import quote
+
 from pyrogram import Client, emoji, filters
+from pyrogram.errors import UserNotParticipant
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, InlineQueryResultCachedDocument
 
-from utils import get_search_results, is_subscribed
-from info import CACHE_TIME, AUTH_USERS, AUTH_CHANNEL, CUSTOM_FILE_CAPTION
+from utils import get_search_results
+from info import CACHE_TIME, SHARE_BUTTON_TEXT, AUTH_USERS, AUTH_CHANNEL
 
 logger = logging.getLogger(__name__)
 cache_time = 0 if AUTH_USERS or AUTH_CHANNEL else CACHE_TIME
@@ -16,7 +19,7 @@ async def answer(bot, query):
     if AUTH_CHANNEL and not await is_subscribed(bot, query):
         await query.answer(results=[],
                            cache_time=0,
-                           switch_pm_text='You have to subscribe my channel to use the bot',
+                           switch_pm_text='You have to subscribe channel',
                            switch_pm_parameter="subscribe")
         return
 
@@ -30,29 +33,18 @@ async def answer(bot, query):
         file_type = None
 
     offset = int(query.offset or 0)
-    reply_markup = get_reply_markup(query=string)
+    reply_markup = get_reply_markup(bot.username, query=string)
     files, next_offset = await get_search_results(string,
                                                   file_type=file_type,
                                                   max_results=10,
                                                   offset=offset)
 
     for file in files:
-        title=file.file_name
-        size=file.file_size
-        f_caption=file.caption
-        if CUSTOM_FILE_CAPTION:
-            try:
-                f_caption=CUSTOM_FILE_CAPTION.format(file_name=title, file_size=size, file_caption=f_caption)
-            except Exception as e:
-                print(e)
-                f_caption=f_caption
-        if f_caption is None:
-            f_caption = f"{file.file_name}"
         results.append(
             InlineQueryResultCachedDocument(
                 title=file.file_name,
                 file_id=file.file_id,
-                caption=f_caption,
+                caption=file.caption or "",
                 description=f'Size: {get_size(file.file_size)}\nType: {file.file_type}',
                 reply_markup=reply_markup))
 
@@ -61,19 +53,11 @@ async def answer(bot, query):
         if string:
             switch_pm_text += f" for {string}"
 
-        try:
-            await query.answer(results=results,
-                           is_personal = True,
+        await query.answer(results=results,
                            cache_time=cache_time,
                            switch_pm_text=switch_pm_text,
                            switch_pm_parameter="start",
                            next_offset=str(next_offset))
-        except Exception as e:
-            logging.exception(str(e))
-            await query.answer(results=[], is_personal=True,
-                           cache_time=cache_time,
-                           switch_pm_text=str(e)[:63],
-                           switch_pm_parameter="error")
     else:
 
         switch_pm_text = f'{emoji.CROSS_MARK} No results'
@@ -81,19 +65,17 @@ async def answer(bot, query):
             switch_pm_text += f' for "{string}"'
 
         await query.answer(results=[],
-                           is_personal = True,
                            cache_time=cache_time,
                            switch_pm_text=switch_pm_text,
                            switch_pm_parameter="okay")
 
 
-def get_reply_markup(query):
-    buttons = [
-        [
-            InlineKeyboardButton('Search again', switch_inline_query_current_chat=query),
-            InlineKeyboardButton('More Bots', url='https://t.me/subin_works/122')
-        ]
-        ]
+def get_reply_markup(username, query):
+    url = 't.me/share/url?url=' + quote(SHARE_BUTTON_TEXT.format(username=username))
+    buttons = [[
+        InlineKeyboardButton('Search again', switch_inline_query_current_chat=query),
+        InlineKeyboardButton('Share bot', url=url),
+    ]]
     return InlineKeyboardMarkup(buttons)
 
 
@@ -108,3 +90,16 @@ def get_size(size):
         size /= 1024.0
     return "%.2f %s" % (size, units[i])
 
+
+async def is_subscribed(bot, query):
+    try:
+        user = await bot.get_chat_member(AUTH_CHANNEL, query.from_user.id)
+    except UserNotParticipant:
+        pass
+    except Exception as e:
+        logger.exception(e)
+    else:
+        if not user.status == 'kicked':
+            return True
+
+    return False
